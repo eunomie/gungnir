2feb912ff5964a4df9cbcb2ed04e609e08f8cfbb
Yves Brissaud <yves@sogilis.com>
2014-02-10 11:58:31 +0100

    write todo in git

--- app.rb
+++ app.rb
@@ -1,8 +1,35 @@
 require "bundler"
 Bundler.require
+require 'yaml'
 include Sprockets::Helpers
 
 REPO_PATH = "git-backend"
+
+def write(repo, content, path, message)
+  commit(repo, tree(repo, blob(repo, content), path), message)
+end
+
+def blob(repo, content)
+  repo.write(content, :blob)
+end
+
+def tree(repo, oid, path)
+  index = Rugged::Index.new
+  index.add(:path => path, :oid => oid, :mode => 0100644)
+  index.write_tree(repo)
+end
+
+def commit(repo, tree, message)
+  options = {}
+  options[:tree] = tree
+  options[:author] = { :email => "contact@sogilis.com", :name => 'sogilis', :time => Time.now }
+  options[:committer] = { :email => "contact@sogilis.com", :name => 'sogilis', :time => Time.now }
+  options[:message] ||= message
+  options[:parents] = repo.empty? ? [] : [ repo.head.target ].compact
+  options[:update_ref] = 'HEAD'
+
+  Rugged::Commit.create(repo, options)
+end
 
 class Item
   attr_accessor :id
@@ -28,9 +55,13 @@ class Item
     @@items
   end
 
-  def self.create(content, time)
-    @@items.push(Item.new(content, time))
-    @@items
+  def self.create(content, time, repo)
+    item = Item.new(content, time)
+    write(repo, item.to_yaml, "items/#{item.id}", "new item")
+  end
+
+  def to_yaml
+    {"id" => @id, "content" => @content, "time" => @time, "done" => @done}.to_yaml
   end
 end
 
@@ -55,7 +86,7 @@ class App < Sinatra::Base
   end
 
   post '/new' do
-    Item.create(params[:content], Time.now)
+    Item.create(params[:content], Time.now, repo)
     redirect '/'
   end
 end
